0dab6a8cc3b3b171c28b265d1220711191c30be3
BOYA LIU <boyaliu@BOYAs-MacBook-Air.local>
2023-10-01 17:21:12 +0800

    fix device

--- dann_trainer.py
+++ dann_trainer.py
@@ -134,7 +134,9 @@ class FasterRCNNTrainer(nn.Module):
             self.loc_normalize_std)
         
         # NOTE it's all zero because now it only support for batch=1 now
-        sample_roi_index = t.zeros(len(sample_roi))
+        sample_roi_index = t.zeros(len(sample_roi)).to(opt.device)
+        sample_roi = sample_roi.to(opt.device)
+        
         roi_cls_loc, roi_score = self.faster_rcnn.head(
             features,
             sample_roi,
