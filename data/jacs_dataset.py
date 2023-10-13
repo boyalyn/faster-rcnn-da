19015410e47dad36efd38f577aec9da8ff39a295
BOYA LIU <boyaliu@BOYAs-MacBook-Air.local>
2023-10-13 08:47:28 +0800

    change lr dataset

--- data/jacs_dataset.py
+++ data/jacs_dataset.py
@@ -32,6 +32,7 @@ class JACSDatasetBase(Dataset):
                    "width": img_dict["width"],
                    "instances": []}
             data_dic[key] = val
+            break
 
         for anno_dict in anno_list:
             img_id = anno_dict["image_id"]
@@ -42,6 +43,7 @@ class JACSDatasetBase(Dataset):
             cur_dict = {"bbox": bbox,
                         "cls": cls}
             data_dic[img_id]["instances"].append(cur_dict)
+            break
 
         self.data_dic = data_dic
         self.data_ids = list(data_dic.keys())
