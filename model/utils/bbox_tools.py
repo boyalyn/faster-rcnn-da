bbefa829ed06334f162529dae335e0a085d07050
BOYA LIU <boyaliu@BOYAs-MacBook-Air.local>
2023-10-23 21:38:22 +0800

    learnable scale

--- model/utils/bbox_tools.py
+++ model/utils/bbox_tools.py
@@ -230,6 +230,9 @@ def generate_anchor_base(base_size=16, ratios=[0.5, 1, 2],
     py = base_size / 2.
     px = base_size / 2.
 
+    print("##########")
+    print(len(ratios))
+
     anchor_base = np.zeros((len(ratios) * len(anchor_scales), 4),
                            dtype=np.float32)
     for i in six.moves.range(len(ratios)):
